76fa93c703ae5ca7572c793a0944f29f066e419c
michal.siwek <michal.siwek@lingaro.com>
2017-10-30 09:52:59 +0100

    [Ch.7] Coin flip CLT simulation

--- code/stat_inf_exerc.R
+++ code/stat_inf_exerc.R
@@ -243,6 +243,7 @@ ggplot(data.frame(x=1:n, y=means), aes(x=x, y=y)) +
 ## Central limit theorem
 
 # Die roll simulation
+
 # ver.1 (just for n=10)
 n <- 10; nosim <- 1000
 mu <- sum(1:6)/6; sd <- sqrt(sum((1:6)^2)/6 - mu^2)
@@ -285,6 +286,47 @@ dat <- data.frame(
 g <- ggplot(dat, aes(x = x, fill = size)) + geom_histogram(alpha = .20, binwidth=.3, colour = "black", aes(y = ..density..)) 
 g <- g + stat_function(fun = dnorm, size = 2)
 g + facet_grid(. ~ size)
+
+# Coin flip simulation
+
+# ver.1 (inspired by the previous simulation)
+nosim <- 1000
+mu <- .5; sd <- sqrt(mu * (1 - mu))
+cfunc <- function(x, n) sqrt(n) * (mean(x) - mu) / sd
+dat <- data.frame(
+    x = c(apply(matrix(sample(0:1, nosim*10, replace=T), nosim), 1, cfunc, 10),
+          apply(matrix(sample(0:1, nosim*20, replace=T), nosim), 1, cfunc, 20),
+          apply(matrix(sample(0:1, nosim*30, replace=T), nosim), 1, cfunc, 30)),
+    size = factor(rep(c(10,20,30), rep(nosim, 3))))
+require(ggplot2)
+ggplot(dat, aes(x = x, fill = size)) +
+    geom_histogram(aes(y = ..density..)) + # necessary to display the histogram in terms of density, not counts
+    stat_function(fun = dnorm) + # this shows density of std Normal, so will fit the density scale
+    facet_grid(. ~ size)
+ggplot(dat, aes(x = x, fill = size)) +
+    geom_histogram(alpha = .20, binwidth=.3, colour = "black", aes(y = ..density..)) +
+    stat_function(fun = dnorm, size = 2) +
+    facet_grid(. ~ size)
+
+# ver. official
+nosim <- 1000
+cfunc <- function(x, n) 2 * sqrt(n) * (mean(x) - 0.5) 
+dat <- data.frame(
+    x = c(apply(matrix(sample(0:1, nosim * 10, replace = TRUE), 
+                       nosim), 1, cfunc, 10),
+          apply(matrix(sample(0:1, nosim * 20, replace = TRUE), 
+                       nosim), 1, cfunc, 20),
+          apply(matrix(sample(0:1, nosim * 30, replace = TRUE), 
+                       nosim), 1, cfunc, 30)
+    ),
+    size = factor(rep(c(10, 20, 30), rep(nosim, 3))))
+g <- ggplot(dat, aes(x = x, fill = size)) + geom_histogram(binwidth=.3, colour = "black", aes(y = ..density..)) 
+g <- g + stat_function(fun = dnorm, size = 2)
+g + facet_grid(. ~ size)
+
+# p=0.9
+
+
 
 
 
